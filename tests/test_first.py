38ec93fb429d64a87fa75c7742c054b385046421
Pallav <pallavgarg04@gmail.com>
2020-10-30 11:50:14 +0530

    Jenkinsfile added

--- tests/test_first.py
+++ tests/test_first.py
@@ -7,5 +7,12 @@ def test_sum():
     assert sums(3,4) == 7
     assert sums("a","b") == "ab"
 
+def test_passes_but_bad_style():
+    try:
+        assert sums("a", 5) or sums(5, "b")
+        assert False
+    except TypeError:
+        assert True
+
 #if __name__ == '__main__':
 #    test_calc()
