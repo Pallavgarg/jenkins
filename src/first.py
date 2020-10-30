c77e73eda6eecbb7343fa2eacc172244a19c7bd0
Pallav <pallavgarg04@gmail.com>
2020-10-30 11:40:56 +0530

    Jenkinsfile added

--- src/first.py
+++ src/first.py
@@ -2,5 +2,12 @@ def calc():
     return "Hello World!"
 
 
+def sums(a,b):
+    return a + b
+
+
 if __name__ == '__main__':
     calc()
+    a= 2
+    b= 3
+    sums(a,b)
--- tests/test_first.py
+++ tests/test_first.py
@@ -1,7 +1,11 @@
-from src.first import calc
+from src.first import calc,sums
 
 def test_calc():
     assert calc() == "Hello World!"
+
+def test_sum():
+    assert sums(3,4) == 7
+    assert sums("a","b") == "ab"
 
 #if __name__ == '__main__':
 #    test_calc()
